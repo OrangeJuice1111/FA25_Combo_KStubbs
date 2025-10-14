0a20310e6374468449a986c5279dcec704cd6050
Kyle Stubbs <KMStubbs26@outlook.com>
2025-10-14 10:34:38 -0600

    DND

---
+++ DnDCharecter/src/CharacterRoler.java
@@ -0,0 +1,81 @@
+
+public class CharacterRoler {
+	private int str = rollChar();
+	private int dex = rollChar();
+	private int con = rollChar();
+	private int intl = rollChar();
+	private int wis = rollChar();
+	private int cha = rollChar();
+
+	int[] stats = { str, dex, con, intl, wis, cha };
+
+	public CharacterRoler() {
+		super();
+		this.str = str;
+		this.dex = dex;
+		this.con = con;
+		this.intl = intl;
+		this.wis = wis;
+		this.cha = cha;
+	}
+
+	private int rollChar() {
+		int sum = 0;
+		int low = 6;
+		for (int i = 0; i < 4; i++) {
+			int roll = Dice.roll(6);
+			sum += roll;
+			if (roll < low) {
+				low = roll;
+			}
+		}
+		sum -= low;
+		return sum;
+	}
+
+	private int findMod(int stat) {
+		int mod = ((stat - 10) / 2);
+		return mod;
+	}
+
+	private String recType() {
+		int high = stats[0];
+		String ret = "";
+		for (int i = 0; i < stats.length; i++) {
+			if (stats[i] > high)
+				high = i;
+		}
+		switch (high) {
+			case 0:
+				ret = "Barbarian";
+				break;
+			case 1:
+				ret = "Rouge";
+				break;
+			case 2:
+				ret = "Barbarian";
+				break;
+			case 3:
+				ret = "Wizard";
+				break;
+			case 4:
+				ret = "Druid";
+				break;
+			case 5:
+				ret = "Bard";
+				break;	
+			default:
+				ret = "NPC";
+		}
+		return ret;
+	}
+
+	@Override
+	public String toString() {
+		return "CharacterRoler [str=" + str + " Mod [" + findMod(str) + "], dex=" + dex + " Mod [" + findMod(dex)
+				+ "], con=" + con + " Mod [" + findMod(con) + "], intl=" + intl + " Mod [" + findMod(intl) + "], wis="
+				+ wis + " Mod [" + findMod(wis) + "], cha=" + cha + " Mod [" + findMod(cha) + "]" + recType();
+		
+	}
+
+}
--- DnDCharecter/src/Charecter_Driver.java
+++ DnDCharecter/src/Charecter_Driver.java
@@ -1,44 +1,48 @@
 import java.util.Scanner;
 
 public class Charecter_Driver {
+	
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		System.out.println("what is your charecters name?");
-		String name = input.next();
+		CharacterRoler me = new CharacterRoler();
+		System.out.println(me);
 		
-		System.out.println("How many hit points?");
-		int hp = input.nextInt();
-		
-		System.out.println("How much power?");
-		int power = input.nextInt();
-		
-		System.out.println("What is there armor class?");
-		int AC = input.nextInt();
-		
-		Character bob = new Character(hp,name,power,AC);
-		
-		System.out.println(bob.toString());
-		Character monster = new Character();
-		
-		monster.setName("Bugaboo");
-		monster.setPower(40);
-		monster.setHealth(80);
-		monster.setArmor(10);
-		
-		System.out.println(bob.toString());
-		System.out.println("\n" + monster);
-		System.out.println();
-		Weapon sword = new Weapon(28, "Sword");
-		System.out.println("Sword");
-		
-		bob.setWeapon(35, "Long_Sword");
-		monster.setWeapon(25, "claws");
-		System.out.println(bob.toString());
-		System.out.println(monster.toString());
-		
-		Character harpy = new Character(100, "Harpy", 75,25, 35, "Talons");
-		
+//		Scanner input = new Scanner(System.in);
+//		System.out.println("what is your charecters name?");
+//		String name = input.next();
+//		
+//		System.out.println("How many hit points?");
+//		int hp = input.nextInt();
+//		
+//		System.out.println("How much power?");
+//		int power = input.nextInt();
+//		
+//		System.out.println("What is there armor class?");
+//		int AC = input.nextInt();
+//		
+//		Character bob = new Character(hp,name,power,AC);
+//		
+//		System.out.println(bob.toString());
+//		Character monster = new Character();
+//		
+//		monster.setName("Bugaboo");
+//		monster.setPower(40);
+//		monster.setHealth(80);
+//		monster.setArmor(10);
+//		
+//		System.out.println(bob.toString());
+//		System.out.println("\n" + monster);
+//		System.out.println();
+//		Weapon sword = new Weapon(28, "Sword");
+//		System.out.println("Sword");
+//		
+//		bob.setWeapon(35, "Long_Sword");
+//		monster.setWeapon(25, "claws");
+//		System.out.println(bob.toString());
+//		System.out.println(monster.toString());
+//		
+//		Character harpy = new Character(100, "Harpy", 75,25, 35, "Talons");
+//		
 //		weapon.setName("Sword");
 //		weapon.setDamage(20);
 		
